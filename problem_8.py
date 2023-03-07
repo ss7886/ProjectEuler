bae10c9e70db4cce985ffd2dc1efc9aefcf89ce2
Samuel Sanft <sanft@princeton.edu>
2023-03-06 23:55:06 -0500

    Problem 8

---
+++ problem_8.py
@@ -0,0 +1,36 @@
+"""
+The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+
+(See problem_8.txt)
+
+Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this
+product?
+
+https://projecteuler.net/problem=8
+
+Solution by Sam Sanft
+"""
+
+
+def problem_8():
+    f = open("problem_8.txt")
+    num_str = ""
+    for line in f:
+        num_str += line[:-1]
+
+    factors = 13
+    n = len(num_str)
+    assert n == 1000
+    champ = -1
+    for i in range(n - factors + 1):
+        product = 1
+        for c in num_str[i:i+factors]:
+            product *= int(c)
+        if product > champ:
+            champ = product
+    print(champ)
+
+
+
+if __name__ == "__main__":
+    problem_8()
