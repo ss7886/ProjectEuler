3b1136618a33ffa73bd6f949d7d3790acb41c3a0
Samuel Sanft <sanft@princeton.edu>
2023-03-20 12:29:40 -0600

    Problem 18 (edit)

--- problem_18.py
+++ problem_18.py
@@ -15,7 +15,7 @@ Find the maximum total from top to bottom of the triangle below:
 
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 
-https://projecteuler.net/problem=17
+https://projecteuler.net/problem=18
 
 Solution by Sam Sanft
 """
