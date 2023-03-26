9cd2c2501a2018abd966ab2fdd0c3cfeac540c7d
Samuel Sanft <sanft@princeton.edu>
2023-03-25 21:30:37 -0400

    Problem 20

---
+++ problem_20.py
@@ -0,0 +1,36 @@
+"""
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+
+https://projecteuler.net/problem=20
+
+Solution by Sam Sanft
+"""
+import math
+
+
+def sum_digits(x):
+    """
+    Returns the sum of the digits of a positive integer x.
+    """
+    str_x = str(x)
+    count = 0
+    for i in range(len(str_x)):
+        count += int(str_x[i])
+    return count
+
+
+def problem_20():
+    max_factor = 100
+    factorial = 1
+    for i in range(max_factor):
+        factorial *= (i + 1)
+    print(sum_digits(factorial))
+
+
+if __name__ == "__main__":
+    problem_20()
